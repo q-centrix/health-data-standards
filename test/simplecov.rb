770456be22c197aad5e136b1b3c9daf046a313b1
Dillon Welch <daw0328@gmail.com>
2015-12-18 16:50:14 -0800

    Remove separate formatter.

--- test/simplecov.rb
+++ test/simplecov.rb
@@ -13,14 +13,3 @@ SimpleCov.start do
   add_group "Utilities", "lib/health-data-standards/util"
   add_group "Validate", "lib/health-data-standards/validate"
 end
-
-class SimpleCov::Formatter::QualityFormatter
-  def format(result)
-    SimpleCov::Formatter::HTMLFormatter.new.format(result)
-    File.open("coverage/covered_percent", "w") do |f|
-      f.puts result.source_files.covered_percent.to_f
-    end
-  end
-end
-
-SimpleCov.formatter = SimpleCov::Formatter::QualityFormatter
